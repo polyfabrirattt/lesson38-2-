e245b4104ed85d2700fd3d556062305a51d5ed00
группы стационара <st@itstep.lan>
2024-01-31 18:35:05 +0300

    Add project files.

---
+++ lesson38(2)/lesson38(2).cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+int sum(int a, int b);
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n";
+	cout << "Hello World!\n";
+	cout << "Hello World!\n";
+
+	cout << "Hello World!\n";
+
+	cout << sum(5, 7) << endl;
+}
